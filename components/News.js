d861e6a38d987a2705253a0f800e2c5f7967c548
draizKUN <benoit.antunes@ecoles-wis.net>
2022-05-28 01:15:03 +0200

    guts

--- components/News.js
+++ components/News.js
@@ -8,27 +8,27 @@ function News() {
       </div>
 
       <div className="mt-[100px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[30px] gap-y-[30px] place-items-center justify-self-center mx-10 lg:mx-40">
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img1.jpg" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">LA KARMINE CORP REMPORTE LES AMAZON EUROPEAN MASTERS ET RÉALISE LE THREE-PEAT</span>
         </div>
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img2.jpg" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">La Karmine Corp crée son roster VALORANT !</span>
         </div>
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img3.png" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">ekWateur DEVIENT PARTENAIRE DE LA KARMINE CORP</span>
         </div>
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img4.png" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">LA KARMINE CORP VOUS PRÉSENTE SON MAILLOT COLLECTOR EUM</span>
         </div>
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img5.jpg" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">DOS AU MUR : le film de la Karmine Corp aux LFL days à Nice</span>
         </div>
-        <div className="relative w-[350px] h-[350px] cursor-pointer">
+        <div className="relative w-[300px] lg:w-[350px] h-[300px] lg:h-[350px] cursor-pointer">
           <img className="opacity-60" src="img6.jpg" alt="" />
           <span className="absolute w-[70%] left-[10px] pl-2 bottom-3 border border-transparent border-l-[#00D8EC] border-4 text-white text-xl font-bold opacity-0 hover:opacity-100">Un nouveau partenaire : NZXT</span>
         </div>
